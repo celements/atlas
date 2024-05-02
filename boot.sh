71df4d03901b181841a7b378486999faf87ea0a9
Marc Sladek <marc@sladek.dev>
2024-05-02 11:32:09 +0200

    chmod env files

--- boot.sh
+++ boot.sh
@@ -4,6 +4,7 @@ set -euo pipefail # strict mode
 ! command -v docker > /dev/null && echo 'docker command missing' && exit 1
 [ ! -f .env ] && echo 'no .env file `cp sample.env .env`' && exit 1
 [ ! -f mongo/.env ] && echo 'no mongo/.env file `cp mongo/sample.env mongo/.env`' && exit 1
+chmod o-rwx .env mongo/.env
 
 compose='docker compose --env-file .env --env-file mongo/.env'
 $compose up -d
